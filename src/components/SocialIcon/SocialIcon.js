266789a74e28948541229b297643bf1b1b04c897
Vicktor Desrony <vldcreation21@gmail.com>
2022-02-22 01:21:51 +0700

    feat: update social icons

--- src/components/SocialIcon/SocialIcon.js
+++ src/components/SocialIcon/SocialIcon.js
@@ -37,7 +37,7 @@ function SocialIcon() {
       <ul>
         <li className="item">
           <a
-            href="https://www.linkedin.com/in/your_username/"
+            href="https://www.linkedin.com/in/vicktordesrony/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -46,7 +46,7 @@ function SocialIcon() {
         </li>
         <li className="item">
           <a
-            href="https://github.com/your_username/"
+            href="https://github.com/vldcreation/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -55,7 +55,7 @@ function SocialIcon() {
         </li>
         <li className="item">
           <a
-            href="https://github.com/your_username/"
+            href="https://github.com/vicktor_desrony/"
             target="_blank"
             rel="noopener noreferrer"
           >
